884b4f9fafee9ce82ff9064ca2bdc245e71582ee
MatiasCalfual <mat.calfual@duocuc.cl>
2026-06-28 17:17:11 -0400

    Add properties and constructor to ServicioTuristico

--- src/Model/ServicioTuristico.java
+++ src/Model/ServicioTuristico.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class ServicioTuristico {
-}
+
+    protected String nombre;
+    protected int duracionHoras;
+
+    public ServicioTuristico(String nombre, int duracionHoras) {
+        this.nombre = nombre;
+        this.duracionHoras = duracionHoras;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDuracionHoras() {
+        return duracionHoras;
+    }
